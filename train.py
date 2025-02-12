e838191a1ac5ef643a03a8dd5fb2c30f4deda575
João Victor Zilio Verzoto <verzoto@gmail.com>
2025-02-12 09:54:45 -0300

    arquivo dataset.yaml

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('yolo11n.pt')
-results = model.train(data='model.yaml', epochs=10, device="mps")
+results = model.train(data='dataset.yaml', epochs=10, device="mps")
